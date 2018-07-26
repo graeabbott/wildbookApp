0c3fcba40a03a19b5808baa2776565c08151bd46
Affan Farid <affanfarid29@gmail.com>
2018-07-26 13:05:42 -0500

    image cropping/rotation fixed

--- MyApplication1/app/src/main/java/com/example/affanfarid/myapplication1/HomeFragment.java
+++ MyApplication1/app/src/main/java/com/example/affanfarid/myapplication1/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.affanfarid.myapplication1;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
@@ -27,7 +28,7 @@ public class HomeFragment extends Fragment {
 
         View myFragmentView = inflater.inflate(R.layout.home_fragment, container, false);
 
-        int num = 1;
+        int num = 7;
 
         retrieveFile(myFragmentView, num);
 
@@ -62,9 +63,61 @@ public class HomeFragment extends Fragment {
 
         Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
 
-        if(bitmap.getWidth() > bitmap.getHeight()){ //portrait
-            bitmap = rotateImage(bitmap, 90);
+        System.out.println("HEIGHT IS: "+ bitmap.getHeight());
+        System.out.println("WIDTH IS: "+ bitmap.getWidth());
+
+        System.out.println("ORIENTATION IS:" + getCameraPhotoOrientation(file));
+
+
+
+        switch (getCameraPhotoOrientation(file)){
+            case 0:
+                bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+                break;
+            case 90:
+                bitmap = rotateImage(bitmap, 90);
+                //bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+                bitmap = Bitmap.createBitmap(bitmap, 0, bitmap.getHeight()/2 - bitmap.getWidth()/2, bitmap.getWidth(), bitmap.getWidth());
+                break;
+            case 180:
+                bitmap = rotateImage(bitmap, 180);
+                bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+                break;
+
+            case 270:
+                bitmap = rotateImage(bitmap, 270);
+                //bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+                bitmap = Bitmap.createBitmap(bitmap, 0, bitmap.getHeight()/2 - bitmap.getWidth()/2, bitmap.getWidth(), bitmap.getWidth());
+                break;
+            default:
+                break;
         }
+
+
+
+        if(bitmap.getWidth() > bitmap.getHeight()){
+
+            System.out.println("TEST1");
+
+            //bitmap = rotateImage(bitmap, 90);
+
+            bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+
+
+            //bitmap = Bitmap.createBitmap(bitmap, 0, bitmap.getHeight()/2 - bitmap.getWidth()/2, bitmap.getWidth(), bitmap.getWidth());
+
+        } else if(bitmap.getHeight() != bitmap.getWidth()){
+
+
+            //bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth()/2 - bitmap.getHeight()/2, 0, bitmap.getHeight(), bitmap.getHeight());
+            bitmap = Bitmap.createBitmap(bitmap, 0, bitmap.getHeight()/2 - bitmap.getWidth()/2, bitmap.getWidth(), bitmap.getWidth());
+
+            //bitmap = rotateImage(bitmap, 90);
+            System.out.println("TEST2");
+        }
+
+
+
         imgView.setImageBitmap(bitmap);
 
     }
@@ -77,5 +130,39 @@ public class HomeFragment extends Fragment {
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
                 matrix, true);
     }
+
+
+    public int getCameraPhotoOrientation( File imageFile){
+        int rotate = 0;
+        try {
+            //context.getContentResolver().notifyChange(imageUri, null);
+            //File imageFile = new File(imagePath);
+
+            ExifInterface exif = new ExifInterface(imageFile.getAbsolutePath());
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+            }
+
+//            Log.i("RotateImage", "Exif orientation: " + orientation);
+//            Log.i("RotateImage", "Rotate value: " + rotate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rotate;
+    }
+
+
+
+
 
 }
